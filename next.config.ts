9c23027a723a090836ac959d25c1b53608488035
DivyaTech18 <2006gdivya@gmail.com>
2025-12-11 15:52:55 +0530

    feat: update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Explicitly disable Turbopack to avoid runtime errors
+  experimental: {
+    turbo: false,
+  },
 };
 
 export default nextConfig;
